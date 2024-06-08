a0ed42d294eec1a7b49cc2a1b007ba1781340464
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2024-06-08 21:36:30 +0530

    Create 01_Variables.js

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+ let accountEmail ="tiwaripuneet626@gmail.com"
+ var accountPassword = "123456"
+ accountCity = "Etawah"
+ let accountstate;
+ //accountID =2 const keyword does'nt change
+ console.log(accountID);
+ accountEmail="tiwari@gmail.com"
+ accountPassword="12234"
+ console.log(accountEmail);
+ console.table([accountID,accountEmail,accountPassword,accountCity,accountstate])
+ //account table make table of each content
+ /*prefer not to use var because of issue 
+ in blockscope and functional scope*/
