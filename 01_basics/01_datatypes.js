a68d37e29c8986ae31e6553d02111376584a6a49
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2024-06-09 08:19:06 +0530

    Create 01_datatypes.js
    data types

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as new version
+//alert(3+3) //we are using nodejs not browser so it show error
+console.log(3+3); //it will print 6 in console
+let nAme="PUNEET" //string datatype
+let age=19 //integer
+let isLoggedIn=false   //boolean datatype
+console.table([nAme,age,isLoggedIn])
+// number=> 2 to the powe 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable is declared but not initialized
+//symbol => unique identifier we used at the react 
+//object => collection of key value pair
+
+//console.log(typeof null); // ouput gives object 
+console.log(typeof undefined); //output is undefined
+
