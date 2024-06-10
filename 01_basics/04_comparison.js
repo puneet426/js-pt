c7752614353a5ff20b626b5c1e41389f71c51312
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2024-06-10 09:28:19 +0530

    Create 04_comparison.js
    compare the different data types

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2==1)
+// console.log(2!=1)
+
+console.log("3">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+console.log(undefined==0)
+console.log(undefined<0)
+console.log(undefined>0)
+
+// === strictly check the value and datatype
+
+console.log("2"===2);
+
