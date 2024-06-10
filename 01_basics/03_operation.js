f61ce236f815bfac6f00b51d872728b918adbf3c
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2024-06-10 09:27:19 +0530

    Create 03_operation.js
    here we solve some operation problems

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,96 @@
+let value=4
+let negvalue = -value
+console.log(negvalue)
+console.log( 5**5)  //5 ki power 5
+console.log(5**-5)  //5 ki power -5
+console.log(5**5**-5)  //5**5=3125  ki power -5
+
+let str1="Hello"
+let str2=" Puneet"
+console.log(str1+str2)  //HelloPuneet
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+4);
+
+console.log((3+4)*5%3)
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+  
+let num1,num2,num3
+num1=num2=num3=3+3
+
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter)
+
+let gamecounter1=100
+gamecounter1++;
+console.log(gamecounter1)
+
+
+
+//********************************Area**************************************** */
+// const PI=Math.PI;
+//  let r = prompt( "enter the radius");
+//  radius = parseFloat(radius); //for input
+//    // Print the area of the circle:
+//    let area = PI*r*r;
+   
+//    // Print the perimeter of the circle:
+//    let perimeter = 2*PI*r
+//    console.log(area)
+//    console.log(perimeter)
+
+/////////////////!SECTION
+// let radius = prompt("Please enter the radius of the circle:");
+
+// // Convert the input to a number
+// radius = parseFloat(radius);
+
+// // Check if the input is a valid number
+// if (isNaN(radius) || radius <= 0) {
+//   alert("Please enter a valid radius.");
+// } else {
+//   // Calculate the area of the circle
+//   let area = Math.PI * Math.pow(radius, 2);
+
+//   // Display the result
+//   alert("The area of the circle is: " + area.toFixed(2));
+// }
+
+
+
+
+//+++++++++++++++++++++++++++++++++++ AREA OF RECTANGLE +++++++++++++++++++++++++++++++
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+// function getArea(length, width) {
+//     let area;
+//     area=length*width;
+    
+//     return area;
+// }
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+// function getPerimeter(length, width) {
+//     let perimeter;
+//     // Write your code here
+//     perimeter= 2*(length+width);
+//     return perimeter;
+// }
