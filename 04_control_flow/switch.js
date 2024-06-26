0cf3ec4ae299c7acc23ddff2e842b3303c945f9d
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2024-06-26 07:39:55 +0530

    Add files via upload

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {               //
+//     case value:              //
+                                //        BASIC SWITCH SYNTAX          //
+//         break;          
+
+//     default:
+//         break;
+// }
+
+const month = 4
+switch (month) {                 // key is the value which we want to check
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+         console.log("feb");
+        
+        break;
+      
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("default")
+        break;
+}
